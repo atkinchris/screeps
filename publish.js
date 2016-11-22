7fa4da5514aef3ec864ca992b8845716257a096e
Chris Atkin <atkinchris@gmail.com>
2016-11-22 22:05:40 +0000

    Add package and linting

--- publish.js
+++ publish.js
@@ -4,20 +4,20 @@ const { username, password } = require('./credentials')
 const data = {
   branch: 'default',
   modules: {
-      main: 'require("hello");',
-      hello: 'console.log("Hello World!");'
-  }
+    main: 'require("hello");',
+    hello: 'console.log("Hello World!");',
+  },
 }
 
 const req = https.request({
-	hostname: 'screeps.com',
-	port: 443,
-	path: '/api/user/code',
-	method: 'POST',
-	auth: `${username}:${password}`,
-	headers: {
-		'Content-Type': 'application/json; charset=utf-8'
-	}
+  hostname: 'screeps.com',
+  port: 443,
+  path: '/api/user/code',
+  method: 'POST',
+  auth: `${username}:${password}`,
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+  },
 })
 
 req.write(JSON.stringify(data))
