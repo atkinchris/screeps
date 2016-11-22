e5cb59edd35a8ab20b6773219e5e30ac2e08fe99
Chris Atkin <atkinchris@gmail.com>
2016-11-22 22:23:19 +0000

    Add Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    bundle: './src/main.js',
+  },
+  output: {
+    filename: '[name].js',
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
+}
