5c53d02be47eb9832fb3032625cabb30d0567a32
Chris Atkin <atkinchris@gmail.com>
2016-11-23 22:01:49 +0000

    Fix repairs

--- src/roles/builder.js
+++ src/roles/builder.js
@@ -7,6 +7,7 @@
 
     const targets = creep.room.find(FIND_CONSTRUCTION_SITES)
 
+
     if (targets.length > 0) {
       if (creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
         creep.moveTo(targets[0])
@@ -15,9 +16,9 @@
       const repairs = creep.room.find(FIND_STRUCTURES, {
         filter: structure => (structure.hits < 5000) && (structure.hits > 0),
       })
-      if (repairs.length) {
-        if (creep.repair(targets[0]) === ERR_NOT_IN_RANGE) {
-          creep.moveTo(targets[0])
+      if (repairs.length > 0) {
+        if (creep.repair(repairs[0]) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(repairs[0])
         }
       }
     }
