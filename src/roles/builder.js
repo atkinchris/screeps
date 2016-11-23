53b53ad93dbd0a5eb3388e27fe643eb8afaa7beb
Chris Atkin <atkinchris@gmail.com>
2016-11-23 21:38:42 +0000

    Add repairs

--- src/roles/builder.js
+++ src/roles/builder.js
@@ -8,6 +8,15 @@
         if (creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
           creep.moveTo(targets[0])
         }
+      } else {
+        const repairs = creep.room.find(FIND_STRUCTURES, {
+          filter: structure => (structure.hits < 5000) && (structure.hits > 0),
+        })
+        if (repairs.length) {
+          if (creep.repair(targets[0]) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(targets[0])
+          }
+        }
       }
     }
   }
