0eddffd68ef63980edee9e3e880651dfdde4ed6f
Chris Atkin <atkinchris@gmail.com>
2016-11-23 12:32:11 +0000

    Aggregate role counts

--- src/main.js
+++ src/main.js
@@ -1,33 +1,27 @@
+const pruneMemory = require('./utils/memory').prune
+
 const roles = {
   harvester: require('./roles/harvester'),
   upgrader: require('./roles/upgrader'),
-}
-
-function pruneMemory() {
-  Memory.creeps = Object.keys(Memory.creeps).reduce((creeps, name) => {
-    if (!Game.creeps[name]) {
-      console.log('Clearing non-existing creep memory:', name)
-      return creeps
-    }
-
-    return Object.assign({}, creeps, { [name]: Memory.creeps[name] })
-  }, {})
 }
 
 function loop() {
   pruneMemory()
   const creeps = Object.keys(Game.creeps).map(name => Game.creeps[name])
 
-  const harvesters = creeps.filter(creep => creep.memory.role === 'harvester')
-  const upgraders = creeps.filter(creep => creep.memory.role === 'upgrader')
-  console.log(`Harvesters: ${harvesters.length}`)
+  const stats = creeps.reduce((out, creep) => {
+    const role = creep.memory.role
+    const count = out[role] ? out[role] + 1 : 1
 
-  if (harvesters.length < 1) {
+    return Object.assign({}, out, { [role]: count })
+  })
+
+  if (stats.harvester < 1) {
     const harvester = Game.spawns.alpha.createCreep([WORK, CARRY, MOVE, MOVE], undefined, { role: 'harvester' })
     console.log(`Spawning new harvester: ${harvester}`)
   }
 
-  if (upgraders.length < 2) {
+  if (stats.upgrader < 2) {
     const upgrader = Game.spawns.alpha.createCreep([WORK, CARRY, MOVE, MOVE], undefined, { role: 'upgrader' })
     console.log(`Spawning new upgrader: ${upgrader}`)
   }
