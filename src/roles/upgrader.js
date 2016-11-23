86f7b8a1e3d8c9e2f2b9b67556fce41a5609bd1d
Chris Atkin <atkinchris@gmail.com>
2016-11-23 22:03:45 +0000

    Improve upgrader capacity

--- src/roles/upgrader.js
+++ src/roles/upgrader.js
@@ -1,5 +1,5 @@
 function run(creep) {
-  if (creep.carry.energy === 0) {
+  if (creep.carry.energy < creep.carryCapacity) {
     const sources = creep.room.find(FIND_SOURCES)
     if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
       creep.moveTo(sources[0])
