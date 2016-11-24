f1cdecd5a39848530cb4e950b286cd44d970717f
Chris Atkin <atkinchris@gmail.com>
2016-11-24 18:47:09 +0000

    Add caching of source positions to memory

--- src/main.js
+++ src/main.js
@@ -13,6 +13,12 @@ function loop() {
   const creeps = Object.keys(Game.creeps).map(name => Game.creeps[name])
   const stats = countRoles(creeps)
 
+  Object.keys(Game.spawns).forEach((key) => {
+    const spawn = Game.spawns[key]
+    const sources = spawn.room.lookForAtArea(LOOK_SOURCES, 0, 0, 49, 49, true)
+    spawn.room.memory.sources = sources.map(({ x, y }) => ({ x, y }))
+  })
+
   Memory.stats = stats
 
   Object.keys(stats).forEach((role) => {
