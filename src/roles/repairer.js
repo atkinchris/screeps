a6c7cbb9950880519e40530aea398164efb1a37e
Chris Atkin <atkinchris@gmail.com>
2016-11-26 19:10:12 +0000

    Fix crashes in builder and repairer

--- src/roles/repairer.js
+++ src/roles/repairer.js
@@ -3,50 +3,27 @@ const roleBuilder = require('./builder')
 module.exports = {
     // a function to run the logic for this role
   run(creep) {
-        // if creep is trying to repair something but has no energy left
     if (creep.memory.working === true && creep.carry.energy === 0) {
-            // switch state
       creep.memory.working = false
-    }
-        // if creep is harvesting energy but is full
-    else if (creep.memory.working === false && creep.carry.energy === creep.carryCapacity) {
-            // switch state
+    } else if (creep.memory.working === false && creep.carry.energy === creep.carryCapacity) {
       creep.memory.working = true
     }
 
-        // if creep is supposed to repair something
     if (creep.memory.working === true) {
-            // find closest structure with less than max hits
-            // Exclude walls because they have way too many max hits and would keep
-            // our repairers busy forever. We have to find a solution for that later.
-      const structure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                // the second argument for findClosestByPath is an object which takes
-                // a property called filter which can be a function
-                // we use the arrow operator to define it
-        filter: s => s.hits < s.hitsMax && s.structureType !== STRUCTURE_WALL,
+      const structure = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+        filter: s => (s.hits < 5000) && (s.hits < s.hitsMax),
       })
 
-            // if we find one
-      if (structure !== undefined) {
-                // try to repair it, if it is out of range
+      if (structure) {
         if (creep.repair(structure) === ERR_NOT_IN_RANGE) {
-                    // move towards it
           creep.moveTo(structure)
         }
-      }
-            // if we can't fine one
-      else {
-                // look for construction sites
+      } else {
         roleBuilder.run(creep)
       }
-    }
-        // if creep is supposed to harvest energy from source
-    else {
-            // find closest source
+    } else {
       const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE)
-            // try to harvest energy, if the source is not in range
       if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
-                // move towards the source
         creep.moveTo(source)
       }
     }
