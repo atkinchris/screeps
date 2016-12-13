5e763ac9250e5780c2a864a8e24333c04ed096ad
Chris Atkin <atkinchris@gmail.com>
2016-12-13 20:51:14 +0000

    Fix missing role counts

--- src/utils/population.js
+++ src/utils/population.js
@@ -10,19 +10,19 @@ function populate(spawn) {
   const energy = spawn.room.energyCapacityAvailable
   let name
 
-  if (counts.harvester < minimums.harvester) {
+  if ((counts.harvester || 0) < minimums.harvester) {
     name = spawn.createCustomCreep(energy, 'harvester')
 
     if (name === ERR_NOT_ENOUGH_ENERGY && counts.harvester === 0) {
       name = spawn.createCustomCreep(spawn.room.energyAvailable, 'harvester')
     }
-  } else if (counts.upgrader < minimums.upgrader) {
+  } else if ((counts.upgrader || 0) < minimums.upgrader) {
     name = spawn.createCustomCreep(energy, 'upgrader')
-  } else if (counts.repairer < minimums.repairer) {
+  } else if ((counts.repairer || 0) < minimums.repairer) {
     name = spawn.createCustomCreep(energy, 'repairer')
-  } else if (counts.builder < minimums.builder) {
+  } else if ((counts.builder || 0) < minimums.builder) {
     name = spawn.createCustomCreep(energy, 'builder')
-  } else if (counts.wallRepairer < minimums.wallRepairer) {
+  } else if ((counts.wallRepairer || 0) < minimums.wallRepairer) {
     name = spawn.createCustomCreep(energy, 'wallRepairer')
   } else {
     name = spawn.createCustomCreep(energy, 'builder')
