3fdad9bbe7a7932b51be9f1482c4dab164daec8c
Chris Atkin <atkinchris@gmail.com>
2016-12-16 08:25:59 +0000

    Fix tower code

--- src/roles/tower.js
+++ src/roles/tower.js
@@ -1,11 +1,10 @@
-function defendRoom(roomName) {
-  const hostiles = Game.rooms[roomName].find(FIND_HOSTILE_CREEPS)
+function defendRoom(room) {
+  const hostiles = room.find(FIND_HOSTILE_CREEPS)
 
   if (hostiles.length > 0) {
     const username = hostiles[0].owner.username
-    Game.notify(`User ${username} spotted in room ${roomName}`)
-    const towers = Game.rooms[roomName].find(
-            FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER } })
+    Game.notify(`User ${username} spotted in room ${room.name}`)
+    const towers = room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER } })
     towers.forEach(tower => tower.attack(hostiles[0]))
   }
 }
