0e9a8e09433fcc6ea2cadbf412e39d8eabb70239
Chris Atkin <atkinchris@gmail.com>
2017-01-17 09:23:44 +0000

    Remove notifications from towers

--- src/roles/tower.js
+++ src/roles/tower.js
@@ -2,8 +2,8 @@ function defendRoom(room) {
   const hostiles = room.find(FIND_HOSTILE_CREEPS)
 
   if (hostiles.length > 0) {
-    const username = hostiles[0].owner.username
-    Game.notify(`User ${username} spotted in room ${room.name}`)
+    // const username = hostiles[0].owner.username
+    // Game.notify(`User ${username} spotted in room ${room.name}`)
     const towers = room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER } })
     towers.forEach(tower => tower.attack(hostiles[0]))
   }
